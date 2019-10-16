b32acb1229727d561d6a256eacfdc3117c4ed22d
Yasmim Thasla <yasmim.thasla@gmail.com>
2019-10-16 10:53:56 -0300

    register2

--- half-pugg-front/src/pages/register2.js
+++ half-pugg-front/src/pages/register2.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Button, Comment, Form, Image } from 'semantic-ui-react'
 
 import './register2.css';
-import pugg from '../images/Logo_Pugg.png'; 
-
 export default function({history}) {
 
     const [ descricao, setDescricao ] = useState(''); 
@@ -17,26 +15,23 @@ export default function({history}) {
     return (
         <div className = "register-container">    
             <div className= "register-title">
-                <Link to = "/">
-                    <img src={pugg} width="100" height="100" alt="pugg logo"/>
-                </Link>
                 <h1>Half Pugg</h1>
             </div>
             <div className = "register-inputs">
-            <form onSubmit={handleSubmit}>
+            <form>
                 <ul>
-                    <li>
-                        <input 
-                            placeholder = "Insira uma descrição:"
-                            value = {descricao}
-                            onChange = { e => setDescricao(e.target.value)} 
-                            maxLength = {200}
-                        />
-                    </li>
+                    <div>
+                        <h4>DESCRIÇÃO</h4>
+                        <Form descricao id={"descricao"}>
+                            <Form.TextArea />
+                        </Form>
+                        <h4>IMAGEM</h4>
+
+                    </div>
                 </ul>
             </form>
             </div>
-            <button type="submit" >
+            <button type="submit" onClick={e => handleSubmit(e)} >
                     Sign-up
                 </button>
         </div>
