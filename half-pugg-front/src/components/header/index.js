eacdfdc32b44abd2c8d05bee8a3f0624620480a7
Yasmim Thasla <yasmim.thasla@gmail.com>
2019-10-23 10:43:47 -0300

    Pagina Curriculo
    Almost done

---
+++ half-pugg-front/src/components/header/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+import './styles.css';
+
+export default function header() {
+
+    return(
+        <div>
+            <div className="game-title">
+                <h1>Half Pugg</h1>
+            </div> 
+            <div className="curriculo-title">
+            <h2>Curriculo</h2>
+            </div> 
+        </div>
+    );
+  }
--- half-pugg-front/src/routes.js
+++ half-pugg-front/src/routes.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Login from './pages/login'
 import register from './pages/register'
 import register2 from './pages/register2'
-import curriculo from './pages/curriculo'
+import curriculoT from './pages/curriculoT'
+import connect from './pages/connect'
 
 export default function () {
     return (
@@ -12,7 +13,8 @@ export default function () {
             <Route path="/" exact component={Login}/>
             <Route path="/register" component={register}/>
             <Route path="/bio" component={register2}/>
-            <Route path="/curriculo" component={curriculo}/>
+            <Route path="/curriculo" component={curriculoT}/>
+            <Route path="/connect" component={connect}/>
         </BrowserRouter>
     );
 }
