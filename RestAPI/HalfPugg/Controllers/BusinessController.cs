60c76668672ea8f9adc043db2195d5c077c68a00
Ruannilton <44852912+Ruannilton@users.noreply.github.com>
2019-10-23 07:36:55 -0300

    Added BusinessControler

---
+++ RestAPI/HalfPugg/Controllers/BusinessController.cs
@@ -0,0 +1,37 @@
+﻿using HalfPugg.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using OverwatchAPI;
+using System.Threading.Tasks;
+using System.Web.Http.Results;
+using System.Web.Http.Description;
+
+namespace HalfPugg.Controllers
+{
+    public class BusinessController : ApiController
+    {
+        private HalfPuggContext db = new HalfPuggContext();
+
+        [ResponseType(typeof(IEnumerable<player>))]
+        [Route("api/GetPlayerOw")]
+        [HttpGet]
+        public IHttpActionResult GetPlayerOw(int GameID)
+        {
+            List<string> names = new List<string>();
+            List<region> regions = new List<region>();
+
+            foreach(PlayerGame pg in db.PlayerGames.Where(x=>x.IDGame == GameID))
+            {
+                names.Add(pg.IdAPI);
+                regions.Add(region.us);
+            }
+
+            return Json(OwAPI.GetPlayer(names, regions));
+        }
+
+    }
+}
