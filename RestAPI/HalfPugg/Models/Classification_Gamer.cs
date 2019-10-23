599075306febc0f16b6caad64f85a7590449ae2f
maaira <38172242+maaira@users.noreply.github.com>
2019-10-22 21:04:19 -0300

    Migrations 3.0

--- RestAPI/HalfPugg/Models/Classification_Gamer.cs
+++ RestAPI/HalfPugg/Models/Classification_Gamer.cs
@@ -8,11 +8,8 @@ namespace HalfPugg.Models
 {
     public class Classification_Gamer
     {
-        [Key]
-        public int ID { get; set; }
-        [Required]
-        [StringLength(50)]
-        public string Description { get; set; }
+        [Key] public int ID { get; set; }
+        [Required] [StringLength(50)] public string Description { get; set; }
         
         public DateTime CreateAt { get; set; }
         
--- RestAPI/HalfPugg/Models/GameInGame.cs
+++ RestAPI/HalfPugg/Models/GameInGame.cs
@@ -8,19 +8,11 @@ namespace HalfPugg.Models
 {
     public class GameInGame
     {
-        [Key]
-        [Required]
-        public int ID { get; set; }
-        [StringLength(300)]
-        [Required]
-        public Classification_Gamer Classification { get; set; }
-        [Required]
-        public PlayerGame IdGame { get; set; }
-        [Required]        
-        public float Points { get; set; }
-        
-        public DateTime CreateAt { get; set; }
-        
+        [Key] public int ID { get; set; }
+        [StringLength(300)] [Required] public Classification_Gamer Classification { get; set; }
+        [Required] public PlayerGame IdGame { get; set; }
+        [Required] public float Points { get; set; }        
+        public DateTime CreateAt { get; set; }        
         public DateTime AlteredAt { get; set; }
     }
 }
