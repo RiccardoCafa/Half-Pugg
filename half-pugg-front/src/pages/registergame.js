9aca9f8fb9dd6f9479a2a9f7dcb712d0f36258bd
Yasmim Thasla <yasmim.thasla@gmail.com>
2019-10-28 12:18:53 -0300

    RegisterGame
    incompleto

---
+++ half-pugg-front/src/pages/registergame.js
@@ -0,0 +1,34 @@
+import React, { useState } from 'react';
+import { Button, Comment, Form, TextArea } from 'semantic-ui-react'
+
+import './register2.css';
+export default  function({history}) {
+
+    const [ descricao, setDescricao ] = useState(''); 
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        console.log('descricao: ' + descricao);
+    }
+
+    return (
+        <div className = "login-container">
+            <form> 
+                <h1>Half Pugg</h1>
+                <div>
+                     <h4>Choose a game</h4>
+                     
+
+                 </div>
+                 <div id = {"botoes"}>
+                    <Button.Group id={"botoes"}>
+                        <Button color='green' onClick={e => handleSubmit(e)} >
+                            Próximo
+                        </Button>
+                    </Button.Group>
+                </div>
+            </form>
+        </div>
+    );
+}
--- half-pugg-front/src/routes.js
+++ half-pugg-front/src/routes.js
@@ -6,6 +6,7 @@ import register from './pages/register'
 import register2 from './pages/register2'
 import curriculoT from './pages/curriculoT'
 import connect from './pages/connect'
+import registergame from '.pages/registergame'
 
 export default function () {
     return (
@@ -15,6 +16,7 @@ export default function () {
             <Route path="/bio" component={register2}/>
             <Route path="/curriculo" component={curriculoT}/>
             <Route path="/connect" component={connect}/>
+            <Route path="/registergame" component={registergame}/>
         </BrowserRouter>
     );
 }
