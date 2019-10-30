009817c089bbe4cee177a59332f69214968f1f28
Yasmim Thasla <yasmim.thasla@gmail.com>
2019-10-29 21:53:28 -0300

    registergame

--- half-pugg-front/src/pages/registergame.js
+++ half-pugg-front/src/pages/registergame.js
@@ -18,11 +18,31 @@ export default  function({history}) {
                 <h1>Half Pugg</h1>
                 <div>
                      <h4>Choose a game</h4>
-                     
+                     <div class="ui segment dimmable">
+                            <h3 class="ui header">Overwatch</h3>
+                            <div class="ui small ui small images images">
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image>
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image>
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image> 
+                            </div>
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png" class="ui medium image"></Image>
+                            </div>
+                    <div class="ui segment dimmable">
+                            <h3 class="ui header">League of legends</h3>
+                            <div class="ui small ui small images images">
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image> 
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image>
+                                    <Image src="https://react.semantic-ui.com/images/wireframe/image.png" class="ui image"> </Image>
+                            </div>
+                                    <Image
+                                        src="https://react.semantic-ui.com/images/wireframe/media-paragraph.png"
+                                        class="ui medium image"
+                                    > </Image>
+                            </div>
 
                  </div>
-                 <div id = {"botoes"}>
-                    <Button.Group id={"botoes"}>
+                 <div id = "botoes">
+                    <Button.Group id="botoes">
                         <Button color='green' onClick={e => handleSubmit(e)} >
                             Próximo
                         </Button>
