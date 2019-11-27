d34f1657d1c14989012469d3da3895249f31d94b
Ruannilton <44852912+Ruannilton@users.noreply.github.com>
2019-11-27 11:01:49 -0300

    Update GamerControler.cs

--- RestAPI/HalfPugg/Controllers/GamerControler.cs
+++ RestAPI/HalfPugg/Controllers/GamerControler.cs
@@ -6,8 +6,10 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
+//using System.Web.Mvc;
 using HalfPugg.Models;
 using HalfPugg.TokenJWT;
 using Newtonsoft.Json;
@@ -251,13 +253,13 @@ namespace HalfPugg.Controllers
         }
 
         [System.Web.Mvc.HttpPost]
-        [System.Web.Mvc.AcceptVerbs(HttpVerbs.Post)]
-        public ActionResult UploadFoto(HttpPostedFileBase file)
+        [System.Web.Mvc.AcceptVerbs(System.Web.Mvc.HttpVerbs.Post)]
+        public System.Web.Mvc.ActionResult UploadFoto(HttpPostedFileBase file)
         {
-     
+
             if (file == null)
             {
-                file = this.Request.Files[0];
+            //    file = this.Request.Files[0];
             }
 
             return null;
